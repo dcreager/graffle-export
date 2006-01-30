36acca98ec8830e9f30fccb5e6fe0d033c67bf47
Douglas Creager <dcreager@alum.mit.edu>
2006-01-30 15:54:23 +0000

    Graffle export script handles absolute pathnames correctly

--- graffle.sh
+++ graffle.sh
@@ -14,10 +14,32 @@ else
     exit 1
 fi
 
+if echo "$INPUT_FILE" | grep '^/'; then
+    # The input filename starts with a slash, and is therefore an
+    # absolute pathname.  There is no need to prepend $PWD.
+    INPUT_PATH=$INPUT_FILE
+else
+    # The input filename does not start with a slash, and is therefore
+    # a relative pathname.  We need to prepend $PWD to get an absolute
+    # path.
+    INPUT_PATH=$PWD/$INPUT_FILE
+fi
+
+if echo "$OUTPUT_FILE" | grep '^/'; then
+    # The output filename starts with a slash, and is therefore an
+    # absolute pathname.  There is no need to prepend $PWD.
+    OUTPUT_PATH=$OUTPUT_FILE
+else
+    # The output filename does not start with a slash, and is
+    # therefore a relative pathname.  We need to prepend $PWD to get
+    # an absolute path.
+    OUTPUT_PATH=$PWD/$OUTPUT_FILE
+fi
+
 DIR=`dirname $0`
 
 echo Format = $FORMAT
-echo Input = $PWD/$INPUT_FILE
-echo Output = $PWD/$OUTPUT_FILE
+echo Input = $INPUT_PATH
+echo Output = $OUTPUT_PATH
 
-osascript $DIR/graffle.scpt "OmniGraffle" "$FORMAT" "$PWD/$INPUT_FILE" "$PWD/$OUTPUT_FILE"
+osascript $DIR/graffle.scpt "OmniGraffle" "$FORMAT" "$INPUT_PATH" "$OUTPUT_PATH"
